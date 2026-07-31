dc2c4568856ef43bb70edac5b42ce5298d4eac65
haekang-lee <haekang10@gmail.com>
2026-07-31 13:52:52 +0900

    modify models/invoke.py

--- models/invoke.py
+++ models/invoke.py
@@ -1,31 +1,26 @@
 from omegaconf import DictConfig
-from openai import AsyncOpenAI, base_url
+from openai import AsyncOpenAI
 
 async def invoke_api(cfg: DictConfig, prompt: str, system_prompt: str) -> str:
-    """
-    gpt-oss-120b 모델 비동기로 호출하는 함수
-    """
-    base_url = cfg.serve.online.url + cfg.serve.online.model
-
-    async with AsyncOpenAI(base_url=base_url, api_key="not-needed") as client:
+    
+    base_url = cfg.serve.online.base_url
+    
+    async with AsyncOpenAI(base_url=base_url, api_key=cfg.serve.online.api_key) as client:
         try:
             response = await client.responses.create(
-                model="/models",
+                model=cfg.serve.online.model,
                 instructions=system_prompt,
                 input=prompt,
-
                 temperature=cfg.serve.online.temperature,
                 top_p=cfg.serve.online.top_p,
-
                 reasoning={
                     "effort": cfg.serve.online.reasoning
                 },
                 max_output_tokens=cfg.serve.online.max_output_tokens,
             )
-
+            
             return response.output_text
-
+    
         except Exception as e:
+            # 에러 발생시 해당 응답 반환
             return f"Failure: API calling: {e}"
-
-
